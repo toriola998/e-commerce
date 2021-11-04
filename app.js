1aab3ff5938ae90e92f14f788910d74df7c1db18
Toriola Faidat <faaedah@gmail.com>
2021-11-04 12:23:24 +0100

    completed functionalities (excluding carousel slider)

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 const menu = document.querySelector('#menu');
 const links = document.querySelector('nav');
 const closeIcon = document.querySelector('#close-icon');
-//const body = document.querySelector('.header')
-//const overlay = document.querySelector('.overlay')
+
+const productPhotos = document.querySelectorAll('.product-photo')
+const carouselPhoto = document.querySelector('.carousel-photo')
+const overlay = document.querySelector('.overlay')
+const carousel = document.querySelector('#carousel')
+const carousel2 = document.querySelector('.carousel2')  //the product div below overlay. display-none by default
+const stackOver = document.querySelectorAll('.stack-over')
+
+const add = document.querySelector('#add');
+const minus = document.querySelector('#minus');
+const cartNumber = document.querySelector('#cart-number')
+const cartDetails = document.querySelector('.cart-details')  //.parentElement
+const cartAlert = document.getElementById('cart-alert');
+
+const addToCartBtn = document.getElementById('add-to-cart-btn')
+const closeOverlay = document.getElementById('close-overlay-icon')
+
+const cartBox = document.querySelector('.cart-box')
+const deleteBtn = document.getElementById('delete-btn')
+const cartImage = document.getElementById('cart')
+
+let navLinks = document.querySelectorAll('.nav-link');
+let numValue = document.querySelector('#num');
+let noOfItems = document.querySelector('.no-of-items')
+let totalPrice = document.querySelector('.total-price')
+let num = 0;
 
 //Toggle to menu hamburger
 menu.addEventListener('click', function(){
 links.classList.toggle('show-links');
-//overlay.style.display = "block"
-//body.classList.add('overlay')
 });
 
 //Close the side bar once the close icon gets clicked
@@ -16,7 +38,6 @@ closeIcon.addEventListener('click', function(){
     links.classList.remove('show-links');
 });
 
-let navLinks = document.querySelectorAll('.nav-link');
 //event listener for each links
 navLinks.forEach(link => {  
   link.addEventListener('click', () => {
@@ -24,22 +45,13 @@ navLinks.forEach(link => {
   }); 
 });
 
-const productPhotos = document.querySelectorAll('.product-photo')
-const carouselPhoto = document.querySelector('.carousel-photo')
 let myProductArray = [...productPhotos]; //convert nodelist to an array
-
 //change thumbnails to hero-images when clicked on.
 myProductArray.forEach(function(item){
   item.addEventListener('click', function(e){
     carouselPhoto.src = e.target.src;
   })
 })
-
-const overlay = document.querySelector('.overlay')
-const carousel = document.querySelector('#carousel')
-const carousel2 = document.querySelector('.carousel2')  //the product div below overlay. display-none by default
-const stackOver = document.querySelectorAll('.stack-over')
-console.log(stackOver)
 
 //make product full the page when clicked on.
 carouselPhoto.addEventListener('click', function(){
@@ -53,19 +65,44 @@ carouselPhoto.addEventListener('click', function(){
   }
 })
  
-const add = document.querySelector('#add');
-const minus = document.querySelector('#minus');
-const cartNumber = document.querySelector('#cart-number')
-let numValue = document.querySelector('#num');
-const cartAlert = document.getElementById('cart-alert');
-let noOfItems = document.querySelector('.no-of-items')
-let num = 0;
+const multiply = function (a, b){
+  return a * b;
+}
+
+const deleteImage = ``
+//console.log(deleteImage)
+
+//cartDetails.innerHTML = '';
+cartDetails.innerHTML = '<p>Your cart is empty</p>'
 
 add.addEventListener('click', () => {
   num++
   numValue.innerHTML = num;
   cartNumber.innerHTML = num; 
-  noOfItems.textContent = num;
+  //noOfItems.textContent = num;
+
+  let calc = parseInt(multiply(125, num))
+  //console.log(calc);
+  //totalPrice.textContent = `$${calc}.00`;
+  //noOfItems.textContent = num
+
+  if(numValue.innerHTML >= 1){
+    cartDetails.innerHTML = `<div>
+                                <div class="flex">
+                                    <img src="" class="cart-product">
+                                    <div>
+                                        <p>Autumn Limited Edition...</p>
+                                        <p>$125.00 * 
+                                            <span class="no-of-items">${num}</span>
+                                            <span class="total-price">$${calc}.00</span></p>
+                                            <img src="./images/icon-delete.svg" id="delete-btn"/>
+                                    </div>
+
+                                </div>
+                                <button id="checkout">Checkout</button>
+                              </div>`
+          
+  }
 })
 
 minus.addEventListener('click', () => {
@@ -73,17 +110,17 @@ minus.addEventListener('click', () => {
     num--;
     numValue.innerHTML = num;
     cartNumber.innerHTML = num;
-    noOfItems.textContent = num;
   }
+ // noOfItems.textContent = num;
 })
 
-
-const addToCartBtn = document.getElementById('add-to-cart-btn')
 addToCartBtn.addEventListener('click', () => {
   cartAlert.style.display = 'flex';
+
+  
+  
 } )
 
-const closeOverlay = document.getElementById('close-overlay-icon')
 closeOverlay.addEventListener('click', () => {
   overlay.style.display = "none";
   carousel2.style.display = "none";
@@ -94,18 +131,28 @@ closeOverlay.addEventListener('click', () => {
   }
 })
 
-const cartBox = document.querySelector('.cart-box')
-const totalPrice = document.querySelector('.total-price')
-const deleteBtn = document.getElementById('delete-btn')
-
-const cartImage = document.getElementById('cart')
-
 cartImage.addEventListener('click', () => {
   cartBox.classList.toggle('cart-box-active')
 })
 
+//const myDelete = 
+//console.log(myDelete)
+ 
+cartDetails.addEventListener('click', (e) => {
+  cartDetails.firstElementChild.firstElementChild.lastElementChild
+  if(e.target.id = 'delete-btn'){
+    cartDetails.innerHTML = '';
+    cartDetails.innerHTML = '<p>Your cart is empty</p>';
+    console.log('red')
+    cartAlert.style.display = "none"
+  }
+   
+    
+  })
 
-noOfItems.textContent = numValue.innerHTML;
+
+
+
   
 
 
