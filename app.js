e91a4df29e3b49d15e1ddc2270d067712898c5ce
Toriola Faidat <faaedah@gmail.com>
2021-12-04 14:49:17 +0100

    added mobile carousel

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ const carousel = document.querySelector('#carousel')
 const carousel2 = document.querySelector('.carousel2')  //the product div below overlay. display-none by default
 const stackOver = document.querySelectorAll('.stack-over')
 
-const add = document.querySelector('#add');
-const minus = document.querySelector('#minus');
 const cartNumber = document.querySelector('#cart-number')
 const cartDetails = document.querySelector('.cart-details')  //.parentElement
 const cartAlert = document.getElementById('cart-alert');
@@ -53,7 +51,7 @@ myProductArray.forEach(function(item){
   })
 })
 
-//make product full the page when clicked on.
+/*make product full the page when clicked on.
 carouselPhoto.addEventListener('click', function(){
   overlay.style.display = "block";
   carousel.classList.add('carousel-active');
@@ -63,62 +61,45 @@ carouselPhoto.addEventListener('click', function(){
     stackOver[i].style.display = 'flex';
     stackOver[i].style.zIndex = 99999;
   }
-})
+})*/
+
 
 //calculation for product price multiplied by number of product picked
 const multiply = function (a, b){
-  return a * b;
+    return a * b;
 }
-
 //The default empty state of the cart box
 cartDetails.innerHTML = '<p class="empty-cart">Your cart is empty</p>'
+  
+const changeCartNumber = [...document.querySelectorAll('.change-cart-num')]
+changeCartNumber.forEach( (changeBtn) => {
+  changeBtn.addEventListener('click', (e) => {
+    let calc = parseInt(multiply(125, num + 1))
+      if (e.target.id === 'add'){
+        num++
+        numValue.innerHTML = num;
+        cartNumber.innerHTML = num; 
+      }else {
+        num--;
+        numValue.innerHTML = num;
+        cartNumber.innerHTML = num;
+      }
 
-
-//when a user adds products
-add.addEventListener('click', () => {
-  let calc = parseInt(multiply(125, num+1))
-  num++
-  numValue.innerHTML = num;
-  cartNumber.innerHTML = num; 
-
-  if(numValue.innerHTML >= 1){
-    cartDetails.innerHTML = `<div>
-                                <div class="flex">
-                                  <div class="flex">
-                                    <img src="./images/image-product-1.jpg" class="cart-product">
-                                    <p>Autumn Limited Edition...
-                                      $125.00 * 
-                                      <span class="no-of-items">${num}</span>
-                                      <span class="total-price">$${calc}.00</span></p>       
-                                  </div>
-                                  <img src="./images/icon-delete.svg" id="delete-btn"/>
-                                </div>
-                                <button id="checkout">Checkout</button>
-                              </div>`      
-  }
-})
-
-//when a user reduces the number of products
-minus.addEventListener('click', () => {
-  let calc = parseInt(multiply(125, num))
-  if(numValue.innerHTML >= 1){
-    num--;
-    numValue.innerHTML = num;
-    cartNumber.innerHTML = num;
-    cartDetails.innerHTML = `<div>
-                                <div class="flex">
-                                  <div class="flex">
-                                    <img src="./images/image-product-1.jpg" class="cart-product">
-                                    <p>Autumn Limited Edition...
-                                      $125.00 * 
-                                      <span class="no-of-items">${num}</span>
-                                      <span class="total-price">$${calc}.00</span></p>       
-                                  </div>
-                                  <img src="./images/icon-delete.svg" id="delete-btn"/>
-                                </div>
-                                <button id="checkout">Checkout</button>
-                              </div>`      
-  }
+      if(numValue.innerHTML >= 1){
+        cartDetails.innerHTML = `<div>
+                                    <div class="flex">
+                                      <div class="flex">
+                                        <img src="./images/image-product-1.jpg" class="cart-product">
+                                        <p>Autumn Limited Edition...
+                                          $125.00 * 
+                                          <span class="no-of-items">${num}</span>
+                                          <span class="total-price">$${calc}.00</span></p>       
+                                      </div>
+                                      <img src="./images/icon-delete.svg" id="delete-btn"/>
+                                    </div>
+                                    <button id="checkout">Checkout</button>
+                                  </div>`  
+  }})
 })
 
 //update cart box when a user clicks on add-to-cart btn
@@ -148,7 +129,61 @@ cartDetails.addEventListener('click', (e) => {
   if(e.target.id == 'delete-btn'){         //if that exact element isn't targeted, the event listener will add to the whole family (parent, child, sibling)
     cartDetails.innerHTML = '';
     cartDetails.innerHTML = '<p class="empty-cart">Your cart is empty</p>';
-    console.log('red')
     cartAlert.style.display = "none"
   }  
 })
+
+
+const info = [
+    {
+        id: 1,
+        img: "./images/image-product-1.jpg",
+    },
+
+    {
+        id: 2,
+        img: "./images/image-product-2.jpg",
+    },
+
+    {   
+        id: 3,
+        img: "./images/image-product-3.jpg",
+    },
+
+    {   
+        id: 3,
+        img: "./images/image-product-4.jpg",
+    }
+]
+
+const carouselBtn = [...document.querySelectorAll('.circle-wrapper')]
+const mobileCarouselPhoto = document.querySelector('.mobile-carousel-photo')
+//Set starting point
+let currentItem = 0;
+//Add event listener to the window that implies when the document loads
+window.addEventListener('DOMContentLoaded', () => {
+    showNext(currentItem)
+})
+//show details based on items
+function showNext (each) {
+    const item = info[each];  //variable of each item index info[0]
+    mobileCarouselPhoto.src = item.img;
+}
+// show next item
+carouselBtn.forEach( (sliderBtn) => {
+    sliderBtn.addEventListener('click', (e) => {
+        if(e.target.id === 'previous') {
+            currentItem++;
+            if (currentItem > info.length - 1) {     //when your current item (num of clicks) gets bigger than array lenght, there'll be an error cos no item will be found, then you have to minus 1 from your arrays lenght bacause the lenght will be greater than the index, number starts from 0!  
+                currentItem = 0;
+            }
+            showNext(currentItem)
+        } else {
+            currentItem++;
+            if (currentItem > info.length - 1) {     //when your current item (num of clicks) gets bigger than array lenght, there'll be an error cos no item will be found, then you have to minus 1 from your arrays lenght bacause the lenght will be greater than the index, number starts from 0!  
+                currentItem = 0;
+            }
+            showNext(currentItem)
+        }
+    }) 
+}) 
